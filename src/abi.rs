8db3f7e38056abfa05106a7009de8b3d1820f11a
Arvin Morawej <arvin.morawej@gmail.com>
2024-11-03 01:04:01 -0400

    Add uncx V3

--- src/abi.rs
+++ src/abi.rs
@@ -1,8 +1,3 @@
 use alloy::sol;
 
-sol!(
-    contract LogDecoder {
-        // UNCX V2
-        event onDeposit(address lpToken, address user, uint256 amount, uint256 lockDate, uint256 unlockDate);
-    }
-);
+sol!(LogDecoder, "./abis.json");
--- src/filter.rs
+++ src/filter.rs
@@ -8,9 +8,16 @@ pub fn build_locked_liquidity_filter() -> Filter {
 pub fn decode_it(log: &Log) -> Option<Address> {
     // Decode the log
     match *log.topic0()? {
+        // UNCX V2
         LogDecoder::onDeposit::SIGNATURE_HASH => {
             if let Ok(data) = LogDecoder::onDeposit::decode_log(&log.inner, false) {
                 return Some(data.lpToken);
+            }
+        }
+        // UNCX V3
+        LogDecoder::onLock::SIGNATURE_HASH => {
+            if let Ok(data) = LogDecoder::onLock::decode_log(&log.inner, false) {
+                return Some(data.poolAddress);
             }
         }
         _ => return None,
